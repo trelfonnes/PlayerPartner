b2728f8175bb71eb826a8e3f16d421d6cf3a12c8
trelfonnes <trel.fonnes@gmail.com>
2023-06-22 15:40:11 -0600

    Solved the Inventory issue with being unable to access runtime instances of PlayerSOData. Made Inventory Mediator a prefab with a list where all PlayerSODatas are dropped in. Used for and foreach loops to iterate over the list of Datas. If the inventory item used meets various conditions, it is applied appropriately. Also made it so partner stats are more congruent without being coupled.

--- Assets/__Game/Scripts/Menu/Inventory/InventoryMediator.cs
+++ Assets/__Game/Scripts/Menu/Inventory/InventoryMediator.cs
@@ -2,68 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryMediator: DataReferenceInheritor
+public class InventoryMediator: MonoBehaviour
 {
-  
 
-    protected override void Awake()
-    {
-        base.Awake();
-        
+    [SerializeField] List<PlayerSOData> Datas = new List<PlayerSOData>();
 
-
-    }
-
+    
     // not sure how to get all partner data referenced other than hard reference.
     //this system works purely with prefabs, nothing in the actual scene.
 
 
     public void IncreaseStamina(float amount)
     {
-        if (partner1SOData)
+        if (Datas != null)
         {
-            partner1SOData.Stamina = Mathf.Clamp(partner1SOData.Stamina + amount, 0, partner1SOData.MaxStamina);
-            partner2SOData.Stamina = Mathf.Clamp(partner2SOData.Stamina + amount, 0, partner2SOData.MaxStamina);
-            partner3SOData.Stamina = Mathf.Clamp(partner3SOData.Stamina + amount, 0, partner3SOData.MaxStamina);
-
+            foreach (PlayerSOData item in Datas)
+            {
+                item.Stamina = Mathf.Clamp(item.Stamina + amount, 0, item.MaxStamina);
+            }
+            
         }
     }
     public void IncreasePartnerHealth(float amount)
     {
-        if (partner1SOData)
+        if (Datas != null)
         {
-            partner1SOData.CurrentHealth = Mathf.Clamp(partner1SOData.CurrentHealth + amount, 0, partner1SOData.MaxHealth);
-            partner2SOData.CurrentHealth = Mathf.Clamp(partner2SOData.CurrentHealth + amount, 0, partner2SOData.MaxHealth);
-            partner3SOData.CurrentHealth = Mathf.Clamp(partner3SOData.CurrentHealth + amount, 0, partner3SOData.MaxHealth);
-        }
+
+            for (int i = 0; i < Datas.Count; i++)
+            {
+                if (i != 0) // 0 is the first slot in the list which will always be for the player
+                {
+                    Datas[i].CurrentHealth = Mathf.Clamp(Datas[i].CurrentHealth + amount, 0, Datas[i].MaxHealth);
+                }
+
+            }
+        }    
         
     }
     public void IncreasePlayerHealth(float amount)
     {
         Debug.Log("Health UP");
-        if (playerSOData)
+        if (Datas != null)
         {
-            playerSOData.CurrentHealth = Mathf.Clamp(playerSOData.CurrentHealth + amount, 0, playerSOData.MaxHealth);
-
+            for (int i = 0; i < Datas.Count; i++)
+            {
+                if (i == 0)
+                {
+                    Datas[i].CurrentHealth = Mathf.Clamp(Datas[i].CurrentHealth + amount, 0, Datas[i].MaxHealth);
+                }
+            }
         }
     }
     public void HealSick( bool sick)
     {
-
-        if (partner1SOData)
+        if (Datas != null)
         {
-            partner1SOData.IsSick = sick;
-            partner2SOData.IsSick = sick;
-            partner3SOData.IsSick = sick;
+
+            foreach (PlayerSOData item in Datas)
+            {
+                item.IsSick = sick;
+            }
         }
     }
     public void HealInjured(bool injured)
     {
-        if (partner1SOData)
+        if (Datas != null)
         {
-            partner1SOData.IsInjured = injured;    
-            partner2SOData.IsInjured = injured;    
-            partner3SOData.IsInjured = injured;    
+            foreach (PlayerSOData item in Datas)
+            {
+                item.IsInjured = injured;
+            }
+
         }
     }
    
